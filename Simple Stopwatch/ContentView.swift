7f69802c1db1a1329df8708b1b37cad5a1ed7b4d
Lch#3181 <34583022+Lch3181@users.noreply.github.com>
2022-03-31 19:13:20 -0500

    Initial Commit

---
+++ Simple Stopwatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simple Stopwatch
+//
+//  Created by Mitchell Lam on 3/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple Stopwatch/Simple_StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_StopwatchApp.swift
+//  Simple Stopwatch
+//
+//  Created by Mitchell Lam on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
